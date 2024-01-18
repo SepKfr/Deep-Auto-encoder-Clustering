6023a21d7032d0cc3d96c4189d2662409fbe6100
sepidekoohfar <sepideh.koohfar@unh.edu>
2024-01-18 12:14:08 -0600

    adding optim

--- train.py
+++ train.py
@@ -59,7 +59,7 @@ class Train:
         parser.add_argument("--max_encoder_length", type=int, default=192)
         parser.add_argument("--max_train_sample", type=int, default=32000)
         parser.add_argument("--max_test_sample", type=int, default=3840)
-        parser.add_argument("--batch_size", type=int, default=356)
+        parser.add_argument("--batch_size", type=int, default=256)
         parser.add_argument("--data_path", type=str, default='~/research/Corruption-resilient-Forecasting-Models/solar.csv')
         parser.add_argument('--cluster', choices=['yes', 'no'], default='yes',
                             help='Enable or disable a feature (choices: yes, no)')
