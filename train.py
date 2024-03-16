3333b637caf36652406a90918fbb291258685413
sepidekoohfar <sepideh.koohfar@unh.edu>
2024-03-16 16:45:30 -0500

    adding Knns as clustering

--- train.py
+++ train.py
@@ -44,7 +44,7 @@ class Train:
         parser.add_argument("--pred_len", type=int, default=24)
         parser.add_argument("--max_train_sample", type=int, default=66560)
         parser.add_argument("--max_test_sample", type=int, default=10240)
-        parser.add_argument("--batch_size", type=int, default=1024)
+        parser.add_argument("--batch_size", type=int, default=512)
         parser.add_argument("--data_path", type=str, default='User_id.csv')
         parser.add_argument('--cluster', choices=['yes', 'no'], default='no',
                             help='Enable or disable a feature (choices: yes, no)')
