06d558ae96dd2a74de8e81ae7b631668bda87933
sepidekoohfar <sepideh.koohfar@unh.edu>
2024-03-23 19:03:22 -0500

    only pick hight value storm events

--- train.py
+++ train.py
@@ -45,8 +45,8 @@ class Train:
         parser.add_argument("--attn_type", type=str, default='ATA')
         parser.add_argument("--max_encoder_length", type=int, default=192)
         parser.add_argument("--pred_len", type=int, default=24)
-        parser.add_argument("--max_train_sample", type=int, default=1024)
-        parser.add_argument("--max_test_sample", type=int, default=256)
+        parser.add_argument("--max_train_sample", type=int, default=3200)
+        parser.add_argument("--max_test_sample", type=int, default=320)
         parser.add_argument("--batch_size", type=int, default=32)
         parser.add_argument("--data_path", type=str, default='watershed.csv')
         parser.add_argument('--cluster', choices=['yes', 'no'], default='no',
