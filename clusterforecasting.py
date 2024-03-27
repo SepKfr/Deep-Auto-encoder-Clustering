df876495e09a8dc79ed33025f1ab7e82908787c3
sepidekoohfar <sepideh.koohfar@unh.edu>
2024-03-27 18:02:55 -0500

    only pick hight value storm events

--- clusterforecasting.py
+++ clusterforecasting.py
@@ -148,7 +148,7 @@ class ClusterForecasting(nn.Module):
         self.d_model = d_model
         self.input_size = input_size
         self.time_proj = 100
-        self.num_clusters = 5
+        self.num_clusters = 4
 
     def forward(self, x, y=None):
 
