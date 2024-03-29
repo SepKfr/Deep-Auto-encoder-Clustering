a706d8255fad94152a707bfec9c6e0fdc720fe6d
sepidekoohfar <sepideh.koohfar@unh.edu>
2024-03-28 19:20:20 -0500

    KNN for clustering on synthetic data

--- train.py
+++ train.py
@@ -46,7 +46,7 @@ class Train:
         parser.add_argument("--attn_type", type=str, default='ATA')
         parser.add_argument("--max_encoder_length", type=int, default=100)
         parser.add_argument("--pred_len", type=int, default=24)
-        parser.add_argument("--max_train_sample", type=int, default=3840)
+        parser.add_argument("--max_train_sample", type=int, default=512)
         parser.add_argument("--max_test_sample", type=int, default=128)
         parser.add_argument("--batch_size", type=int, default=64)
         parser.add_argument("--data_path", type=str, default='watershed.csv')
