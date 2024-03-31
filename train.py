253e75ad31a5e2cd8918230a08e0a84efbf48c58
sepidekoohfar <sepideh.koohfar@unh.edu>
2024-03-31 17:06:32 -0400

    adding var for loss

--- train.py
+++ train.py
@@ -39,7 +39,7 @@ class Train:
     def __init__(self):
 
         parser = argparse.ArgumentParser(description="train args")
-        parser.add_argument("--exp_name", type=str, default="synthetic")
+        parser.add_argument("--exp_name", type=str, default="User_id")
         parser.add_argument("--model_name", type=str, default="basic_attn")
         parser.add_argument("--num_epochs", type=int, default=1)
         parser.add_argument("--n_trials", type=int, default=10)
@@ -47,7 +47,7 @@ class Train:
         parser.add_argument("--attn_type", type=str, default='ATA')
         parser.add_argument("--max_encoder_length", type=int, default=24)
         parser.add_argument("--pred_len", type=int, default=24)
-        parser.add_argument("--max_train_sample", type=int, default=8)
+        parser.add_argument("--max_train_sample", type=int, default=128)
         parser.add_argument("--max_test_sample", type=int, default=8)
         parser.add_argument("--batch_size", type=int, default=8)
         parser.add_argument("--var", type=int, default=1)
