8a3451a05a7b6c92acd8f6d300f8fb4b82c9ceb0
sepidekoohfar <sepideh.koohfar@unh.edu>
2024-04-02 18:11:28 -0400

    new loss as rec loss

--- synthetic_data.py
+++ synthetic_data.py
@@ -58,12 +58,12 @@ class SyntheticDataLoader:
     def get_synthetic_samples(self):
 
         # Training data is 100 points in [0,1] inclusive regularly spaced
-        train_x = torch.linspace(0, 1, 24).view(1, -1, 1).repeat(4, 1, 1)
+        train_x = torch.linspace(0, 1, 100).view(1, -1, 1).repeat(4, 1, 1)
         # True functions are sin(2pi x), cos(2pi x), sin(pi x), cos(pi x)
-        sin_y = torch.sin(train_x[0] * (2 * math.pi)) + 0.5 * torch.rand(1, 24, 1)
-        sin_y_short = torch.sin(train_x[0] * math.pi) + 0.5 * torch.rand(1, 24, 1)
-        cos_y = torch.cos(train_x[0] * (2 * math.pi)) + 0.5 * torch.rand(1, 24, 1)
-        cos_y_short = torch.cos(train_x[0] * math.pi) + 0.5 * torch.rand(1, 24, 1)
+        sin_y = torch.sin(train_x[0] * (2 * math.pi)) + 0.5 * torch.rand(1, 100, 1)
+        sin_y_short = torch.sin(train_x[0] * math.pi) + 0.5 * torch.rand(1, 100, 1)
+        cos_y = torch.cos(train_x[0] * (2 * math.pi)) + 0.5 * torch.rand(1, 100, 1)
+        cos_y_short = torch.cos(train_x[0] * math.pi) + 0.5 * torch.rand(1, 100, 1)
         train_y = torch.cat((sin_y, sin_y_short, cos_y, cos_y_short)).squeeze(-1)
 
         # We will use the simplest form of GP model, exact inference
@@ -115,16 +115,16 @@ class SyntheticDataLoader:
 
         with torch.no_grad():
 
-            test_x = torch.linspace(0, 1, 24).view(1, -1, 1).repeat(4, 1, 1)
+            test_x = torch.linspace(0, 1, 100).view(1, -1, 1).repeat(4, 1, 1)
 
-            for i in range(512):
+            for i in range(10240):
 
                 observed_pred = likelihood(model(test_x)).sample()
                 # Get mean
                 mean = observed_pred.detach().cpu()
                 mean = mean.reshape(4, -1)
                 label = torch.tensor([0, 1, 2, 3])
-                label = label.unsqueeze(-1).repeat(1, 24)
+                label = label.unsqueeze(-1).repeat(1, 100)
                 labels.append(label)
                 samples.append(mean)
 
