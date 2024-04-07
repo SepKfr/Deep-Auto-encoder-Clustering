73884669ab7c392672cdb41ad9e853b4ea30d04b
sepidekoohfar <sepideh.koohfar@unh.edu>
2024-04-07 13:59:09 -0400

    adding eICU data

--- psychology_data_formatter.py
+++ psychology_data_formatter.py
@@ -48,10 +48,6 @@ for id, df in lab.groupby("patientunitstayid"):
         map = df_nurse[df_nurse["nursingchartcelltypevallabel"] == "MAP (mmHg)"][["nursingchartvalue", "time"]]
         respiratory = df_nurse[df_nurse["nursingchartcelltypevallabel"] == "Respiratory Rate"][["nursingchartvalue", "time"]]
 
-        temp = temp.iloc[:len(df_lab)]
-        map = map.iloc[:len(df_lab)]
-        respiratory = respiratory.iloc[:len(df_lab)]
-
         variables = {'hco3': hco3.values,
                      'creatinine': creatinine.values,
                      'potassium': potassium.values,
@@ -62,8 +58,8 @@ for id, df in lab.groupby("patientunitstayid"):
                      'time': lab_time.values,
                      'id': df_lab["patientunitstayid"].values}
 
-        df_new = pd.DataFrame(variables)
-        df_new.index = np.arange(len(df_lab))
+        # df_new = pd.DataFrame(variables)
+        # df_new.index = np.arange(len(df_lab))
 
         hours = [6, 12, 24]
 
@@ -75,45 +71,46 @@ for id, df in lab.groupby("patientunitstayid"):
                         if range_min <= val < range_max:
                             apache_score[i] += score
 
-        min_time = df_new['time'].min()
-
-        six_hours = df_new.loc[(df_new['time'] >= min_time) & (df_new['time'] <= 6+min_time)]
-        twelve_hours = df_new.loc[(df_new['time'] >= min_time) & (df_new['time'] <= 12+min_time)]
-        twenty_four_hours = df_new.loc[(df_new['time'] >= min_time) & (df_new['time'] <= 24+min_time)]
-
-        apache_6 = apache_score[six_hours.index]
-        apache_12 = apache_score[twelve_hours.index]
-        apache_24 = apache_score[twenty_four_hours.index]
-
-        a_score_6 = max(apache_6)
-        a_score_12 = max(apache_12)
-        a_score_24 = max(apache_24)
-
-        six_hours["apache"] = a_score_6
-        twelve_hours["apache"] = a_score_12
-        twenty_four_hours["apache"] = apache_24
-
-        list_patients_6.append(six_hours)
-        list_patients_12.append(twelve_hours)
-        list_patients_24.append(twenty_four_hours)
-
-
-patients_6 = pd.concat(list_patients_6, ignore_index=True)
-patients_12 = pd.concat(list_patients_12, ignore_index=True)
-patients_24 = pd.concat(list_patients_24, ignore_index=True)
-
-patients_6 = patients_6.sort_values(by='time')
-patients_6 = patients_6.dropna()
-
-patients_12 = patients_12.sort_values(by='time')
-patients_12 = patients_12.dropna()
-
-patients_24 = patients_24.sort_values(by='time')
-patients_24 = patients_24.dropna()
-
-patients_6.to_csv("patients_6.csv")
-patients_12.to_csv("patients_12.csv")
-patients_24.to_csv("patients_24.csv")
+        print(apache_score)
+#         min_time = df_new['time'].min()
+#
+#         six_hours = df_new.loc[(df_new['time'] >= min_time) & (df_new['time'] <= 6+min_time)]
+#         twelve_hours = df_new.loc[(df_new['time'] >= min_time) & (df_new['time'] <= 12+min_time)]
+#         twenty_four_hours = df_new.loc[(df_new['time'] >= min_time) & (df_new['time'] <= 24+min_time)]
+#
+#         apache_6 = apache_score[six_hours.index]
+#         apache_12 = apache_score[twelve_hours.index]
+#         apache_24 = apache_score[twenty_four_hours.index]
+#
+#         a_score_6 = max(apache_6)
+#         a_score_12 = max(apache_12)
+#         a_score_24 = max(apache_24)
+#
+#         six_hours["apache"] = a_score_6
+#         twelve_hours["apache"] = a_score_12
+#         twenty_four_hours["apache"] = apache_24
+#
+#         list_patients_6.append(six_hours)
+#         list_patients_12.append(twelve_hours)
+#         list_patients_24.append(twenty_four_hours)
+#
+#
+# patients_6 = pd.concat(list_patients_6, ignore_index=True)
+# patients_12 = pd.concat(list_patients_12, ignore_index=True)
+# patients_24 = pd.concat(list_patients_24, ignore_index=True)
+#
+# patients_6 = patients_6.sort_values(by='time')
+# patients_6 = patients_6.dropna()
+#
+# patients_12 = patients_12.sort_values(by='time')
+# patients_12 = patients_12.dropna()
+#
+# patients_24 = patients_24.sort_values(by='time')
+# patients_24 = patients_24.dropna()
+#
+# patients_6.to_csv("patients_6.csv")
+# patients_12.to_csv("patients_12.csv")
+# patients_24.to_csv("patients_24.csv")
 
 
 
