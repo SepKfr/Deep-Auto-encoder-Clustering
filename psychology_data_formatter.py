cf26e45631c2994bbd55826c455bc7304024d580
sepidekoohfar <sepideh.koohfar@unh.edu>
2024-04-08 13:44:15 -0400

    adding eICU data

--- psychology_data_formatter.py
+++ psychology_data_formatter.py
@@ -6,7 +6,6 @@ nurseCharting = pd.read_csv("nurseCharting.csv")
 lab = pd.read_csv("lab.csv")
 vitalPeriodic = pd.read_csv("vitalPeriodic.csv")
 
-
 df_list = []
 
 scoring_criteria = {'map': [(0, 49, 4), (50, 69, 2), (110, 129, 2), (130, 159, 3), (160, 1000, 4)],
@@ -35,22 +34,40 @@ for id, df in tqdm(lab.groupby("patientunitstayid"), desc='eICU processing'):
 
     vitalPeriodic = vitalPeriodic[vitalPeriodic["patientunitstayid"] == id].sort_values(by='observationoffset')
 
-    lab_time = df_lab["labresultrevisedoffset"] * time_factor
-    nurse_time = df_nurse["nursingchartoffset"] * time_factor
-    vitalPeriodic_time = vitalPeriodic["observationoffset"] * time_factor
+    lab_time = df_lab["labresultrevisedoffset"]
+    nurse_time = df_nurse["nursingchartoffset"]
+    vitalPeriodic_time = vitalPeriodic["observationoffset"]
+
+    min_lab_time = lab_time.min()
+    min_nurse_time = nurse_time.min()
+    min_vital_time = vitalPeriodic_time.min()
+
+    lab_time += abs(min_lab_time)
+    nurse_time += abs(min_nurse_time)
+    vitalPeriodic_time += abs(min_vital_time)
 
     df_lab['time'] = lab_time
     df_nurse['time'] = nurse_time
     vitalPeriodic['time'] = vitalPeriodic_time
 
-    hco3 = df_lab[df_lab["labname"] == "HCO3"][["labresult", "time"]]
-    creatinine = df_lab[df_lab["labname"] == "creatinine"][["labresult", "time"]]
-    potassium = df_lab[df_lab["labname"] == "potassium"][["labresult", "time"]]
-    sodium = df_lab[df_lab["labname"] == "sodium"][["labresult", "time"]]
-    temp = df_nurse[df_nurse["nursingchartcelltypevallabel"] == "Temperature"][["nursingchartvalue", "time"]]
-    map = df_nurse[df_nurse["nursingchartcelltypevallabel"] == "MAP (mmHg)"][["nursingchartvalue", "time"]]
-    respiratory = df_nurse[df_nurse["nursingchartcelltypevallabel"] == "Respiratory Rate"][["nursingchartvalue", "time"]]
-    heart_rate = vitalPeriodic[["heartrate", "time"]]
+    df_lab = df_lab.groupby("time").first()
+    if len(df_nurse) > 0:
+        df_nurse = df_nurse.groupby("time").first()
+    if len(vitalPeriodic) > 0:
+        vitalPeriodic = vitalPeriodic.groupby("time").first()
+
+    df_lab["t"] = df_lab.index
+    df_nurse["t"] = df_nurse.index
+    vitalPeriodic["t"] = vitalPeriodic.index
+
+    hco3 = df_lab[df_lab["labname"] == "HCO3"][["labresult", "t"]]
+    creatinine = df_lab[df_lab["labname"] == "creatinine"][["labresult", "t"]]
+    potassium = df_lab[df_lab["labname"] == "potassium"][["labresult", "t"]]
+    sodium = df_lab[df_lab["labname"] == "sodium"][["labresult", "t"]]
+    temp = df_nurse[df_nurse["nursingchartcelltypevallabel"] == "Temperature"][["nursingchartvalue", "t"]]
+    map = df_nurse[df_nurse["nursingchartcelltypevallabel"] == "MAP (mmHg)"][["nursingchartvalue", "t"]]
+    respiratory = df_nurse[df_nurse["nursingchartcelltypevallabel"] == "Respiratory Rate"][["nursingchartvalue", "t"]]
+    heart_rate = vitalPeriodic[["heartrate", "t"]]
 
     variables = {'hco3': hco3,
                  'creatinine': creatinine,
@@ -66,25 +83,23 @@ for id, df in tqdm(lab.groupby("patientunitstayid"), desc='eICU processing'):
     hours = [6, 12, 24]
 
     apache_len = max(df_lab.index.max(), df_nurse.index.max(), vitalPeriodic.index.max())
-
     apache_score = np.zeros(apache_len+1)
 
     for variable, df in variables.items():
 
         if variable != "id":
-            df_val = df[df.columns[~df.columns.isin(['time'])]].values.reshape(-1)
+            df_val = df[df.columns[~df.columns.isin(['t'])]].values.reshape(-1)
             df_val = pd.to_numeric(df_val, errors="coerce")
             df_val = pd.DataFrame(df_val)
+            df_val.index = df.index
             df_val = df_val.dropna()
-            df_val = df_val.values.astype(float)
-            for i, val in enumerate(df_val):
+            indices = df_val.index
+            values = df_val.values
+            for ind, val in zip(indices, values):
+
                 for range_min, range_max, score in scoring_criteria[variable]:
                     if range_min <= val < range_max:
-                        apache_score[i] += score
-
-    min_time = lab_time.min()
-    max_time = lab_time.max()
-    time = df_lab[df_lab.columns[df_lab.columns.isin(['time'])]]
+                        apache_score[ind] += score
 
     variables["heart_rate"] = heart_rate
 
@@ -92,57 +107,57 @@ for id, df in tqdm(lab.groupby("patientunitstayid"), desc='eICU processing'):
 
     for variable, df in variables.items():
 
-        df = df.sort_values(by='time')
-        df = df.loc[(df['time'] >= min_time) & (df['time'] <= max_time)]
-        df_e_time = df[df.columns[~df.columns.isin(['time'])]].values
-        df_new = pd.DataFrame(df_e_time, columns=[variable])
-        df_list.append(df_new)
-
-    df_list.append(time)
+        df = df.sort_values(by='t')
+        df_e_time = df[df.columns[~df.columns.isin(['t'])]]
+        df_list.append(df_e_time)
 
     df_new = pd.concat(df_list, axis=1)
-    df_new = df_new.sort_values(by='time')
+    df_new = df_new.sort_index()
+    df_new['t'] = df_new.index * time_factor
+    min_time = df_new['t'].min()
 
-    six_hours = df_new.loc[(df_new['time'] >= min_time) & (df_new['time'] <= 6+min_time)]
-    twelve_hours = df_new.loc[(df_new['time'] >= min_time) & (df_new['time'] <= 12+min_time)]
-    twenty_four_hours = df_new.loc[(df_new['time'] >= min_time) & (df_new['time'] <= 24+min_time)]
+    six_hours = df_new.loc[(df_new['t'] >= min_time) & (df_new['t'] <= 6+min_time)]
+    twelve_hours = df_new.loc[(df_new['t'] >= min_time) & (df_new['t'] <= 12+min_time)]
+    twenty_four_hours = df_new.loc[(df_new['t'] >= min_time) & (df_new['t'] <= 24+min_time)]
 
-    apache_6 = apache_score[six_hours.index]
-    apache_12 = apache_score[twelve_hours.index]
-    apache_24 = apache_score[twenty_four_hours.index]
+    if len(six_hours) > 0:
+        apache_6 = apache_score[six_hours.index]
+        apache_12 = apache_score[twelve_hours.index]
+        apache_24 = apache_score[twenty_four_hours.index]
 
-    a_score_6 = max(apache_6)
-    a_score_12 = max(apache_12)
-    a_score_24 = max(apache_24)
+        a_score_6 = max(apache_6)
+        a_score_12 = max(apache_12)
+        a_score_24 = max(apache_24)
 
-    six_hours = six_hours.copy()
-    six_hours["apache"] = a_score_6
-    six_hours["id"] = id
+        six_hours = six_hours.copy()
+        six_hours["apache"] = a_score_6
+        six_hours["id"] = id
 
-    twelve_hours = twelve_hours.copy()
-    twelve_hours["apache"] = a_score_12
-    twelve_hours["id"] = id
+        twelve_hours = twelve_hours.copy()
+        twelve_hours["apache"] = a_score_12
+        twelve_hours["id"] = id
 
-    twenty_four_hours = twenty_four_hours.copy()
-    twenty_four_hours["apache"] = a_score_24
-    twenty_four_hours["id"] = id
+        twenty_four_hours = twenty_four_hours.copy()
+        twenty_four_hours["apache"] = a_score_24
+        twenty_four_hours["id"] = id
 
-    list_patients_6.append(six_hours)
-    list_patients_12.append(twelve_hours)
-    list_patients_24.append(twenty_four_hours)
+        list_patients_6.append(six_hours)
+        list_patients_12.append(twelve_hours)
+        list_patients_24.append(twenty_four_hours)
 
 
 patients_6 = pd.concat(list_patients_6, ignore_index=True)
 patients_12 = pd.concat(list_patients_12, ignore_index=True)
 patients_24 = pd.concat(list_patients_24, ignore_index=True)
 
-patients_6 = patients_6.sort_values(by='time')
+patients_6 = patients_6.sort_values(by='t')
 patients_6 = patients_6.interpolate(method='linear')
 
-patients_12 = patients_12.sort_values(by='time')
+
+patients_12 = patients_12.sort_values(by='t')
 patients_12 = patients_12.interpolate(method='linear')
 
-patients_24 = patients_24.sort_values(by='time')
+patients_24 = patients_24.sort_values(by='t')
 patients_24 = patients_24.interpolate(method='linear')
 
 patients_6.to_csv("patients_6.csv")
