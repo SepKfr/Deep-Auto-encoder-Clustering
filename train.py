90072dbc0b71e39bdcc06af10467f5c69995c27c
sepidekoohfar <sepideh.koohfar@unh.edu>
2024-04-16 11:40:46 -0400

    adding eICU data

--- train.py
+++ train.py
@@ -151,7 +151,7 @@ class Train:
 
     def train_clustering(self, trial):
 
-        d_model = trial.suggest_categorical("d_model", [64, 32, 128])
+        d_model = trial.suggest_categorical("d_model", [64, 32, 128, 256])
         num_layers = trial.suggest_categorical("num_layers", [1, 3])
         gamma = trial.suggest_categorical("gamma", [0.1, 0.01])
         knns = trial.suggest_categorical("knns", [20, 10, 5])
@@ -303,7 +303,7 @@ class Train:
         tot_nmi_loss = []
         tot_p_loss = []
 
-        d_model_list = [64, 32, 128]
+        d_model_list = [64, 32, 128, 256]
         num_layers_list = [1, 3]
         knn_list = [20, 10, 5]
         gamma = [0.1, 0.01]
