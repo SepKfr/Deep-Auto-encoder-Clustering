cda7fa963305dad1d04f71d5c7a277b81b0a9008
sepidekoohfar <sepideh.koohfar@unh.edu>
2024-05-14 13:22:03 -0500

    higher number of neighbors

--- train.py
+++ train.py
@@ -75,7 +75,7 @@ class Train:
         parser.add_argument("--pred_len", type=int, default=24)
         parser.add_argument("--max_train_sample", type=int, default=-1)
         parser.add_argument("--max_test_sample", type=int, default=-1)
-        parser.add_argument("--batch_size", type=int, default=1024)
+        parser.add_argument("--batch_size", type=int, default=256)
         parser.add_argument("--var", type=int, default=1)
         parser.add_argument("--add_entropy", type=lambda x: str(x).lower() == "true", default=False)
         parser.add_argument("--data_path", type=str, default='watershed.csv')
@@ -185,7 +185,7 @@ class Train:
 
     def train_clustering(self, trial):
 
-        d_model = trial.suggest_categorical("d_model", [32, 64])
+        d_model = trial.suggest_categorical("d_model", [32, 64, 128])
         num_layers = trial.suggest_categorical("num_layers", [1, 3])
         gamma = trial.suggest_categorical("gamma", [0.1, 0.01])
         knns = trial.suggest_categorical("knns", [5, 10, 50])
@@ -346,7 +346,7 @@ class Train:
         tot_nmi_loss = []
         tot_p_loss = []
 
-        d_model_list = [32, 64]
+        d_model_list = [32, 64, 128]
         num_layers_list = [1, 3]
         knn_list = [50, 10, 5]
         gamma = [0.1, 0.01]
