20f62c5da097f7b632621f9fdc8a29513e943ae0
sepidekoohfar <sepideh.koohfar@unh.edu>
2024-05-22 09:38:03 -0500

    deep clustering without mutual learning

--- deepclustering.py
+++ deepclustering.py
@@ -2,98 +2,24 @@ import numpy as np
 import random
 import torch.nn as nn
 import torch
-import torch.nn.functional as F
-from torch.nn import Linear
-import torch.distributions as dist
-from sklearn.decomposition import PCA
-from GMM import GmmFull, GmmDiagonal
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import DotProduct, WhiteKernel
 from modules.transformer import Transformer
-from sklearn.cluster import KMeans
-from torchmetrics.clustering import AdjustedRandScore
+from sklearn import metrics
+from torchmetrics.clustering import AdjustedRandScore, NormalizedMutualInfoScore
+from torchmetrics import Accuracy, F1Score
 from tslearn.metrics import SoftDTWLossPyTorch
-from gpytorch.distributions import MultivariateNormal, MultitaskMultivariateNormal
-from gpytorch.kernels import ScaleKernel, RBFKernel
-from gpytorch.likelihoods import MultitaskGaussianLikelihood
-from gpytorch.means import ConstantMean, LinearMean
-from gpytorch.models.deep_gps import DeepGPLayer, DeepGP
-from gpytorch.variational import VariationalStrategy, MeanFieldVariationalDistribution
-from gpytorch.mlls import DeepApproximateMLL, VariationalELBO
 
-from util_scores import get_scores
+from seed_manager import set_seed
 
 torch.autograd.set_detect_anomaly(True)
 
-torch.manual_seed(1234)
-np.random.seed(1234)
-random.seed(1234)
 
-
-class ToyDeepGPHiddenLayer(DeepGPLayer):
-    def __init__(self, input_dims, output_dims, num_inducing=32, mean_type='linear'):
-
-        if output_dims is None:
-            inducing_points = torch.randn(num_inducing, input_dims)
-            batch_shape = torch.Size([])
-        else:
-            inducing_points = torch.randn(output_dims, num_inducing, input_dims)
-            batch_shape = torch.Size([output_dims])
-
-        variational_distribution = MeanFieldVariationalDistribution(
-            num_inducing_points=num_inducing,
-            batch_shape=batch_shape
-        )
-
-        variational_strategy = VariationalStrategy(
-            self,
-            inducing_points,
-            variational_distribution,
-            learn_inducing_locations=True
-        )
-
-        super(ToyDeepGPHiddenLayer, self).__init__(variational_strategy, input_dims, output_dims)
-
-        if mean_type == 'constant':
-            self.mean_module = ConstantMean(batch_shape=batch_shape)
-        else:
-            self.mean_module = LinearMean(input_dims)
-        self.covar_module = ScaleKernel(
-            RBFKernel(batch_shape=batch_shape, ard_num_dims=input_dims),
-            batch_shape=batch_shape, ard_num_dims=None
-        )
-
-    def forward(self, x):
-        mean_x = self.mean_module(x)
-        covar_x = self.covar_module(x)
-        return MultivariateNormal(mean_x, covar_x)
-
-
-class DeepGPp(DeepGP):
-    def __init__(self, num_hidden_dims, num_inducing):
-
-        hidden_layer = ToyDeepGPHiddenLayer(
-            input_dims=num_hidden_dims,
-            output_dims=num_hidden_dims,
-            mean_type='linear',
-            num_inducing=num_inducing
-        )
-
-        super().__init__()
-
-        self.hidden_layer = hidden_layer
-        self.likelihood = MultitaskGaussianLikelihood(num_tasks=num_hidden_dims)
-
-    def forward(self, inputs):
-
-        dist = self.hidden_layer(inputs)
-        return dist
-
-    def predict(self, x):
-
-        dist = self(x)
-        preds = self.likelihood(dist)
-        preds_mean = preds.mean.mean(0)
-
-        return preds_mean
+def purity_score(y_true, y_pred):
+    # compute contingency matrix (also called confusion matrix)
+    contingency_matrix = metrics.cluster.contingency_matrix(y_true, y_pred)
+    # return purity
+    return np.sum(np.amax(contingency_matrix, axis=0)) / np.sum(contingency_matrix)
 
 
 class Autoencoder(nn.Module):
@@ -126,56 +52,144 @@ class DeepClustering(nn.Module):
 
         super(DeepClustering, self).__init__()
 
+        set_seed(seed)
+
         self.device = device
+        self.add_entropy = add_entropy
 
         self.seq_model = Transformer(input_size=input_size, d_model=d_model,
                                      nheads=nheads, num_layers=num_layers,
                                      attn_type=attn_type, seed=seed, device=device)
+
         self.proj_down = nn.Linear(d_model, input_size)
+        self.proj_to_cluster = nn.Linear(d_model, n_clusters)
+        self.proj_down_seq = nn.Linear(d_model, input_size)
 
         self.pred_len = pred_len
         self.nheads = nheads
         self.batch_size = batch_size
         self.d_model = d_model
         self.input_size = input_size
-        self.time_proj = 100
         self.n_clusters = n_clusters
-        self.knns = knns
+        self.var = var
+        self.k = knns
+        self.gamma = gamma
 
     def forward(self, x, y=None):
 
         s_l = x.shape[1]
+        dec_len = s_l
         if len(x.shape) > 3:
             x = x.reshape(self.batch_size, s_l, -1)
 
-        x_rec = self.seq_model(x)
+        x_enc = self.seq_model(x)
 
-        x_rec = self.proj_down(x_rec)
-        x_rec = x_rec.reshape(self.batch_size, -1)
+        x_enc_re = x_enc.reshape(self.batch_size, -1)
+        attn_score = torch.einsum('bl, cl-> bc', x_enc_re, x_enc_re) / np.sqrt(self.d_model * s_l)
+        mask = torch.zeros_like(attn_score).fill_diagonal_(1).to(torch.bool)
+        attn_score = attn_score.masked_fill(mask, value=-torch.inf)
+        scores = torch.softmax(attn_score, dim=-1)
 
-        dist_2d = torch.cdist(x_rec.unsqueeze(1), x_rec.unsqueeze(0)) ** 2
-        dist_2d = dist_2d.squeeze(1)
+        x_rec = torch.einsum('bd, bc-> bd', x_enc_re, scores)
+        x_rec = x_rec.reshape(x_enc.shape)
+        # x_rec_cluster = self.proj_to_cluster(x_rec)
+        x_rec_proj = self.proj_down(x_rec)
+        # x_seq_proj = self.proj_down_seq(x_enc)
 
-        dist_softmax = torch.softmax(-dist_2d, dim=-1)
-        _, k_nearest = torch.topk(dist_softmax, k=self.knns, dim=-1)
+        # _, top_scores = torch.topk(scores, k=self.k, dim=-1)
+        # x_rec_proj_exp = x_rec_proj.unsqueeze(0).expand(self.batch_size, -1, -1, -1)
+        # x_rec_proj_exp_se = x_rec_proj_exp[torch.arange(self.batch_size)[:, None],
+        #                                    top_scores]
+        #
+        # diff_1 = (torch.diff(x_rec_proj_exp_se, dim=1)**2).mean()
+        # diff_2 = (torch.diff(x_rec_proj_exp_se, dim=2)**2).mean()
 
-        loss = dist_2d.sum()
+        if self.var == 1:
 
-        if y is not None:
+            loss = nn.MSELoss(reduction="none")
+        else:
+            loss = SoftDTWLossPyTorch(gamma=self.gamma)
 
-            y = y[:, 0, :].reshape(-1)
-            y_c = y.unsqueeze(0).expand(self.batch_size, -1)
-            y_c = y_c.squeeze(-2)
+        loss = loss(x_rec_proj, x[:, -dec_len:, :]).mean()
 
-            labels = y_c[torch.arange(self.batch_size)[:, None],
-                         k_nearest]
+        #x_rec = self.proj_down(output_seq)
 
-            labels = labels.reshape(self.batch_size, -1)
+        # diffs = torch.diff(x_rec, dim=1)
+        # kernel = 3
+        # padding = (kernel - 1) // 2
+        # mv_avg = nn.AvgPool1d(kernel_size=kernel, padding=padding, stride=1)(diffs.permute(0, 2, 1)).permute(0, 2, 1)
+        # res = nn.MSELoss()(diffs, mv_avg)
 
-            assigned_labels = torch.mode(labels, dim=-1).values
+        # dist_3d = torch.cdist(x_enc, self.centers, p=2)
+        # _, cluster_ids = torch.min(dist_3d, dim=-1)
+        #
+        # assigned_centroids = self.centers[cluster_ids]
+        #
+        # distances = torch.norm(x_enc - assigned_centroids, dim=1)
+        #
+        # # Compute the mean squared distance
+        # loss = torch.mean(distances ** 2)
 
-            assigned_labels = assigned_labels.reshape(-1)
+        # mask = torch.zeros(self.batch_size, self.batch_size).to(self.device)
+        # mask = mask.fill_diagonal_(1).to(torch.bool)
+        # mask = mask.unsqueeze(-1).repeat(1, 1, s_l)
+        # dist_3d = dist_3d.reshape(self.batch_size, self.batch_size, -1)
+        #
+        # dist_3d = dist_3d.masked_fill(mask, value=0.0)
+        #
+        # # x_rec = torch.einsum('lb, bd -> ld', dist_softmax, output_seq)
+        # # x_rec_re = x_rec.reshape(self.batch_size, -1, self.d_model)
+        # # x_rec_proj = self.proj_down(x_rec_re)
+        # # loss_rec = nn.MSELoss()(x_rec_proj, x)
+        #
+        # knn_tensor, k_nearest = torch.topk(dist_3d, k=self.k, dim=1)
 
-            adj_rand_index, nmi, f1, p_score = get_scores(y, assigned_labels, self.n_clusters, device=self.device)
 
-        return loss, adj_rand_index, nmi, f1, p_score, x_rec
+        # x_rec_expand = x_rec.unsqueeze(0).expand(self.batch_size, -1, -1)
+        # k_nearest_e = k_nearest.unsqueeze(-1).repeat(1, 1, self.d_model)
+        #
+        # selected = x_rec_expand[torch.arange(self.batch_size)[:, None, None],
+        #                         k_nearest_e,
+        #                         torch.arange(self.d_model)[None, None, :]]
+        #
+        # selected = selected.reshape(self.batch_size, self.d_model, -1)
+        #
+        # diff_knns = (torch.diff(selected, dim=-1) ** 2).mean()
+        # diff_steps = (torch.diff(selected, dim=1) ** 2).mean()
+
+        #dist_knn = dist_softmax[torch.arange(self.batch_size)[:, None], k_nearest]
+
+        #loss = loss_rec + diff_steps + diff_knns if self.var == 2 else loss_rec
+
+        # y_en = y
+        y = y[:, 0, :].reshape(-1)
+        y_c = y.unsqueeze(0).expand(self.batch_size, -1)
+
+        _, k_nearest = torch.topk(scores, k=self.k, dim=-1)
+        labels = y_c[torch.arange(self.batch_size)[:, None],
+                     k_nearest]
+
+        assigned_labels = torch.mode(labels, dim=-1).values
+        adj_rand_index = AdjustedRandScore()(assigned_labels.to(torch.long), y.to(torch.long))
+        nmi = NormalizedMutualInfoScore()(assigned_labels.to(torch.long), y.to(torch.long))
+        f1 = F1Score(task='multiclass', num_classes=self.n_clusters).to(self.device)(assigned_labels.to(torch.long), y.to(torch.long))
+        p_score = purity_score(y.to(torch.long).detach().cpu().numpy(), assigned_labels.to(torch.long).detach().cpu().numpy())
+
+        # y_en = y_en[:, -:, :].reshape(-1).to(torch.long)
+        # x_rec_cluster = x_rec_cluster.reshape(-1, self.n_clusters)
+        #
+        # permuted_indexes = torch.randperm(len(y_en))
+        # x_rec_cluster = x_rec_cluster[permuted_indexes]
+        # y_en = y_en[permuted_indexes]
+        # picks = int(np.log2(len(y_en)))
+        # x_rec_cluster = x_rec_cluster[picks]
+        # y_en = y_en[picks]
+        #
+        # cross_loss = nn.CrossEntropyLoss()(x_rec_cluster, y_en)
+
+        # if self.add_entropy:
+        #     tot_loss = loss + cross_loss
+        # else:
+        #
+        tot_loss = loss
+        return tot_loss, adj_rand_index, nmi, f1, p_score, x_rec_proj
